dbc1c99370e174bd30ef7bc27fa0cf4f2a7cfeeb
lijianfei1023 <iOS_dev>
2014-07-14 09:38:16 +0800

    Initial Commit

---
+++ FiyChat/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FiyChat
+//
+//  Created by lijianfei on 14-7-14.
+//  Copyright (c) 2014年 lijianfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LJFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LJFAppDelegate class]));
+    }
+}
---
+++ FiyChatTests/FiyChatTests.m
@@ -0,0 +1,34 @@
+//
+//  FiyChatTests.m
+//  FiyChatTests
+//
+//  Created by lijianfei on 14-7-14.
+//  Copyright (c) 2014年 lijianfei. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FiyChatTests : XCTestCase
+
+@end
+
+@implementation FiyChatTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
